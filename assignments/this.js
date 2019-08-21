aa74d9e8f3577810581b19a1129c32009dddb651
Nathaniel Mosco <natemosco@gmail.com>
2019-08-21 16:46:52 -0400

    Definition given for four principles of 'this' keyword. Below examples coded for Window Binding and Implicit Binding.

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,33 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Binding (when you have the 'this' keyword pointing at the window or global scope)
+* 2. Implicit Binding (When you call something using dot notation the object to the left of the dot is what the 'this' keyword point at for a reference)
+* 3. New keyword Binding (when you have a constructor function, the 'this' keyword gets pointed at the "NEW" object being created by the constructor function)
+* 4. Explicit Binding (when you demand the this keyword be attached to a specific object by using the call or apply method on the object in question)
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// 1.Global Binding:
+console.log(this); //nothing to point at so it targets the console/window
 
-// code example for Window Binding
+// 2.implicit Binding:
+var myRing= {
+    design: "Aztec",
+    accent: "Black Onyx",
+    mainStone: "Larimar",
+    size: "US 9.5"
+    material: "Silver"
+    describe: function(){
+        console.log(`My ${this.design} style ${this.mainStone} and ${this.accent} ring is made of ${this.material} and is a size ${this.size} men's ring.`)
+    }
+}
+myRing.describe();  // here the object being referred to is myRing because it is left of the dot notation in this example.
 
-// Principle 2
+// 3.New Keyword Binding:
 
-// code example for Implicit Binding
 
-// Principle 3
 
-// code example for New Binding
+// 4.Explicit Binding:
 
-// Principle 4
-
-// code example for Explicit Binding
